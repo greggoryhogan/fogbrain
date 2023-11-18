54ef5a0d49d777b148ad403283eaf4294edc029b
Gregg Hogan <21093023+greggoryhogan@users.noreply.github.com>
2023-11-18 12:22:27 -0500

    Close menu onclick

--- wp-content/themes/fogbrain/js/site.js
+++ wp-content/themes/fogbrain/js/site.js
@@ -1,8 +1,23 @@
 (function($) {
     //mobile nav toggle
-    $('.nav-toggle').click(function() {
+    $('.nav-toggle').on('click', function() {
+        if($('body').hasClass('is-active')) {
+            setTimeout(function() {
+                $('body').removeClass('is-active');
+            },300)
+        } else {
+            $('body').addClass('is-active');
+        }
         $('.nav-toggle, header, nav').toggleClass('is-active');
         console.log('toggle');
+    });    
+
+    $(document).on('click', 'body.is-active #Main', function() {
+        setTimeout(function() {
+                $('body').removeClass('is-active');
+        }, 300);
+        
+        $('.nav-toggle, header, nav').removeClass('is-active');
     });    
 
 })(jQuery); // Fully reference jQuery after this point.
